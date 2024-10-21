328d8da1b8ebdb80f4913803441bea77feaf0ba1
PadminiLala <plala0901@gmail.com>
2024-10-21 18:13:35 +0530

    scp test

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,6 +22,7 @@ data "aws_organizations_organizational_units" "root" {
 }
 
 resource "aws_controltower_control" "example" {
+  provider = aws.target
   control_identifier = "arn:aws:controltower:${data.aws_region.current.name}::control/AWS-GR_EC2_VOLUME_INUSE_CHECK"
   target_identifier = [
     for x in data.aws_organizations_organizational_units.root.children :
