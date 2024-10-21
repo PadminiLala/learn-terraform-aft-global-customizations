144ec30d67bf3893e25d35c6f6d77bdbaa186ece
PadminiLala <plala0901@gmail.com>
2024-10-21 19:54:05 +0530

    scp test

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,23 +20,71 @@ data "aws_organizations_organizational_units" "root" {
   parent_id = data.aws_organizations_organization.org.roots[0].id
   provider = aws.target
 }
-
-resource "aws_controltower_control" "example" {
+data "aws_organizations_organizational_units" "ous_depth_1" {
   provider = aws.target
-  control_identifier = "arn:aws:controltower:${data.aws_region.current.name}::control/AWS-GR_EC2_VOLUME_INUSE_CHECK"
-  target_identifier = [
-    for x in data.aws_organizations_organizational_units.root.children :
-    x.arn if x.name == "Infrastructure"
-  ][0]
+  for_each  = toset([for x in data.aws_organizations_organizational_units.root.children : x.id])
+  parent_id = each.key
+  depends_on = [
+    data.aws_organizations_organizational_units.root
+  ]
+}
 
-  parameters {
-    key   = "AllowedRegions"
-    value = jsonencode(["us-east-1"])
+data "aws_organizations_organizational_units" "ous_depth_2" {
+  provider = aws.target
+  for_each  = toset([for y in flatten([for x in data.aws_organizations_organizational_units.ous_depth_1 : x.children]) : y.id])
+  parent_id = each.key
+  depends_on = [
+    data.aws_organizations_organizational_units.ous_depth_1
+  ]
+}
+
+data "aws_organizations_organizational_units" "ous_depth_3" {
+  provider = aws.target
+  for_each  = toset([for y in flatten([for x in data.aws_organizations_organizational_units.ous_depth_2 : x.children]) : y.id])
+  parent_id = each.key
+  depends_on = [
+    data.aws_organizations_organizational_units.ous_depth_2
+  ]
+}
+
+data "aws_organizations_organizational_units" "ous_depth_4" {
+  provider = aws.target
+  for_each  = toset([for y in flatten([for x in data.aws_organizations_organizational_units.ous_depth_3 : x.children]) : y.id])
+  parent_id = each.key
+  depends_on = [
+    data.aws_organizations_organizational_units.ous_depth_3
+  ]
+}
+
+
+locals {
+
+  # Extract Guardrails configuration
+  guardrails_list = flatten([
+    for i in range(0, length(var.controls)) : [
+      for pair in setproduct(element(var.controls, i).control_names, element(var.controls, i).organizational_unit_ids) :
+      { "arn:aws:controltower:${data.aws_region.current.name}::control/${pair[0]}" = pair[1] }
+    ]
+  ])
+
+  ous_depth_1 = [for x in data.aws_organizations_organizational_units.root.children : x]
+  ous_depth_2 = flatten([for x in data.aws_organizations_organizational_units.ous_depth_1 : x.children if length(x.children) != 0])
+  ous_depth_3 = flatten([for x in data.aws_organizations_organizational_units.ous_depth_2 : x.children if length(x.children) != 0])
+  ous_depth_4 = flatten([for x in data.aws_organizations_organizational_units.ous_depth_3 : x.children if length(x.children) != 0])
+  ous_depth_5 = flatten([for x in data.aws_organizations_organizational_units.ous_depth_4 : x.children if length(x.children) != 0])
+
+  # Compute map from OU id to OU arn for the whole organization
+  ous_id_to_arn_map = { for ou in concat(local.ous_depth_1, local.ous_depth_2, local.ous_depth_3, local.ous_depth_4, local.ous_depth_5) :
+    ou.id => ou.arn
   }
 }
-output "root_ou" {
-  value = data.aws_organizations_organizational_units.root
-}
-output "ou" {
-  value = data.aws_organizations_organization.org
+
+resource "aws_controltower_control" "guardrails" {
+  provider = aws.target
+  for_each = { for control in local.guardrails_list : join(":", [keys(control)[0], values(control)[0]]) => [keys(control)[0], values(control)[0]] }
+
+  control_identifier = each.value[0]
+
+  target_identifier = local.ous_id_to_arn_map[each.value[1]]
+
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "controls" {
+
+  type = list(object({
+    control_names           = list(string)
+    organizational_unit_ids = list(string)
+  }))
+
+  description = "Configuration of AWS Control Tower Guardrails for the whole organization"
+}
