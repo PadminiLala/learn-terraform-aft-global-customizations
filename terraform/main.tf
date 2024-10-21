4d1bce5636a54a20af82cc0687808e6d9c7c7458
PadminiLala <plala0901@gmail.com>
2024-10-21 17:18:30 +0530

    ipam test

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,16 +3,16 @@
 
 data "aws_region" "current" {}
 
-data "aws_organizations_organization" "example" {}
+data "aws_organizations_organization" "org" {}
 
-data "aws_organizations_organizational_units" "example" {
-  parent_id = data.aws_organizations_organization.example.roots[0].id
+data "aws_organizations_organizational_units" "root" {
+  parent_id = data.aws_organizations_organization.org.roots[0].id
 }
 
 resource "aws_controltower_control" "example" {
   control_identifier = "arn:aws:controltower:${data.aws_region.current.name}::control/AWS-GR_EC2_VOLUME_INUSE_CHECK"
   target_identifier = [
-    for x in data.aws_organizations_organizational_units.example.children :
+    for x in data.aws_organizations_organizational_units.root.children :
     x.arn if x.name == "Infrastructure"
   ][0]
 
@@ -20,4 +20,10 @@ resource "aws_controltower_control" "example" {
     key   = "AllowedRegions"
     value = jsonencode(["us-east-1"])
   }
+}
+output "root_ou" {
+  value = data.aws_organizations_organizational_units.root
+}
+output "ou" {
+  value = data.aws_organizations_organization.org
 }
