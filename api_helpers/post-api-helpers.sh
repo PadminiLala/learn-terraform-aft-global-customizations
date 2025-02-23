20b008e348b4a1d375cce2abc47989c791336596
PadminiLala <plala0901@gmail.com>
2025-02-23 17:44:46 -0500

    test global bash

--- api_helpers/post-api-helpers.sh
+++ api_helpers/post-api-helpers.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-set -ex 
-echo "Executing Post-API Helpers"
-echo "post-API Helpers executed successfully"
+
+# Function to handle errors
--- api_helpers/pre-api-helpers.sh
+++ api_helpers/pre-api-helpers.sh
@@ -2,3 +2,4 @@
 set -ex 
 echo "Executing Pre-API Helpers"
 echo "Pre-API Helpers executed successfully"
+python ./python/delete_default_vpc.py
