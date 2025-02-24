062eaf2674d47c2248d5ff6f6d9c3be1ce69def9
PadminiLala <plala0901@gmail.com>
2025-02-23 21:48:05 -0500

    test

--- api_helpers/pre-api-helpers.sh
+++ api_helpers/pre-api-helpers.sh
@@ -2,4 +2,4 @@
 set -ex 
 echo "Executing Pre-API Helpers"
 echo "Pre-API Helpers executed successfully"
-python api_helpers/python/source/delete_default_vpc.py
+# python api_helpers/python/source/delete_default_vpc.py
